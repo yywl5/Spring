73a72e28e9e7f2614027f031cce5aa865914c9ae
PotatoB666 <1363964141@qq.com>
2022-04-17 12:25:48 +0800

    增加jstl标签库

--- src/main/java/Controllers/potatob6/Logistic/AdminHandler.java
+++ src/main/java/Controllers/potatob6/Logistic/AdminHandler.java
@@ -7,6 +7,7 @@ import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
